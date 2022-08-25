86be1befd2e73a305c1fc8fccd3abae7ac0f894b
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-08-25 14:52:25 +0100

    fixed plane animations

--- RadarGame/Scripts/Animation.gd
+++ RadarGame/Scripts/Animation.gd
@@ -1,4 +1,11 @@
-extends AnimationPlayer
+extends Spatial
 
-func _process(delta):
-	self.play()
+onready var animationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	if animationPlayer.get_animation_list().size() < 1:
+		return
+
+	var animation = animationPlayer.get_animation_list()[0]
+	print(animationPlayer.get_animation_list())
+	animationPlayer.play(animation)
