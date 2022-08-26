a368393cfbc9a8f77bc101207480ee4263feecbc
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-08-26 12:16:01 +0100

    added a sound manager singelton

--- ReactionTest/Menu/Help.gd
+++ ReactionTest/Menu/Help.gd
@@ -8,4 +8,5 @@ func _input(event: InputEvent) -> void:
 
 func _on_Button_button_up() -> void:
     get_tree().root.get_node("Root").queue_free()
+    SoundManager.playSound("res://ReactionTest/Sounds/BackSound.wav")
     get_tree().change_scene("res://ReactionTest/Scenes/MenuScene.tscn")
