f433dab6a7f3a4ee6538910198356882196d1c8d
Richard Fleming <richard.fleming164@gmail.com>
2022-09-08 14:37:42 +0100

    Updated Color Blind Test Tutorial

--- ReactionTest/Menu/Help.gd
+++ ReactionTest/Menu/Help.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _ready() -> void:
-    $Control/Button.grab_focus()
+	$Control/Button.grab_focus()
 
 func _input(event: InputEvent) -> void:
-    GlobalTimer.reset()
+	GlobalTimer.reset()
 
 func _on_Button_button_up() -> void:
-    get_tree().root.get_node("Root").queue_free()
-    SoundManager.playSound("res://ReactionTest/Sounds/BackSound.wav")
-    get_tree().change_scene("res://ReactionTest/Scenes/MenuScene.tscn")
+	get_tree().root.get_node("Root").queue_free()
+	SoundManager.playSound("res://ReactionTest/Sounds/BackSound.wav")
+	get_tree().change_scene("res://ReactionTest/Scenes/MenuScene.tscn")
